95edaf92dfc6188f27fd6f10e891fc130c4d5802
hoon4041 <simjaehoon3@naver.com>
2025-11-02 15:50:42 +0900

    feat: LottoInfo에 price추가

--- src/main/kotlin/lotto/constant/LottoInfo.kt
+++ src/main/kotlin/lotto/constant/LottoInfo.kt
@@ -1,5 +1,5 @@
 package lotto.constant
 
 enum class LottoInfo(val number: Int) {
-    LOTTO_NUMBER_SIZE(number = 6), LOTTO_NUMBER_MIN(number = 1), LOTTO_NUMBER_MAX(number = 45),
+    LOTTO_NUMBER_SIZE(number = 6), LOTTO_NUMBER_MIN(number = 1), LOTTO_NUMBER_MAX(number = 45), LOTTO_PRICE(number = 1000)
 }
