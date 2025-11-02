2be060c201313655b99936b7f92f5f017b7dfb9a
hoon4041 <simjaehoon3@naver.com>
2025-11-02 13:05:37 +0900

    feat ë¡ë구매가격만í: 로또 구매금액만큼 로또번호를 뽑는 LottoMachine 추가

---
+++ src/main/kotlin/lotto/model/LottoMachine.kt
@@ -0,0 +1,18 @@
+package lotto.model
+
+class LottoMachine(private val lottoNumberGenerator: LottoNumberGenerator) {
+
+    fun pickLotto(): Lotto {
+        val lottoNumber = lottoNumberGenerator.generateLottoNumber()
+        return Lotto(lottoNumber)
+    }
+
+    fun buyLotto(purchasePrice: Long): List<Lotto> {
+        // 1000원 단위로 구매 횟수 계산
+        val count = (purchasePrice / 1000).toInt()
+
+        return List(count) {
+            this.pickLotto()
+        }
+    }
+}
